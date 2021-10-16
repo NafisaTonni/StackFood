ed59bfb448d94a0c31d872f5ebc5171b158092fc
aurk0 <febtesham162125@bscse.uiu.ac.bd>
2021-10-16 19:30:54 +0600

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:stack_food/bottomBar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: StackFood()));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class StackFood extends StatefulWidget {
+  const StackFood({Key? key}) : super(key: key);
+
+  @override
+  _StackFoodState createState() => _StackFoodState();
+}
+
+class _StackFoodState extends State<StackFood> {
+  int _selectedPage = 0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      //backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          '76A eighth avenue, New York, US',
+          style: TextStyle(color: Colors.black26, fontSize: 15),
+        ),
+        leading: Icon(
+          Icons.home_filled,
+          color: Colors.black26,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      bottomNavigationBar: BottomBar(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        onPressed: () {},
+        child: Icon(Icons.shopping_cart_rounded),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
