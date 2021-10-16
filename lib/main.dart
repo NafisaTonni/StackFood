68a6d84d11042d0fcef4ea5b5febd33977d92f57
aurk0 <febtesham162125@bscse.uiu.ac.bd>
2021-10-16 20:51:38 +0600

    sbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:stack_food/bottomBar.dart';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: StackFood()));
@@ -13,15 +12,14 @@ class StackFood extends StatefulWidget {
 }
 
 class _StackFoodState extends State<StackFood> {
-  int _selectedPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(
           '76A eighth avenue, New York, US',
-          style: TextStyle(color: Colors.black26, fontSize: 15),
+          style: TextStyle(color: Colors.black26, fontSize: 13),
         ),
         leading: Icon(
           Icons.home_filled,
@@ -30,13 +28,76 @@ class _StackFoodState extends State<StackFood> {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      bottomNavigationBar: BottomBar(),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 6,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              navItems(items: 0, icon: Icon(Icons.home_filled)),
+              navItems(items: 1, icon: Icon(Icons.favorite)),
+              SizedBox(
+                width: 20,
+              ),
+              navItems(items: 2, icon: Icon(Icons.bookmark)),
+              navItems(items: 3, icon: Icon(Icons.menu_rounded))
+            ],
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange,
         onPressed: () {},
         child: Icon(Icons.shopping_cart_rounded),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
+          child: Column(
+            children: [
+              Material(
+                borderRadius: BorderRadius.circular(5),
+                elevation: 8,
+                child: Container(
+                  //color: Colors.white,
+                  height: 45,
+                  width: 350,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          child: TextField(
+                            decoration: InputDecoration.collapsed(
+                                hintText: 'Search food or restaurant here...',
+                                hintStyle: TextStyle(
+                                    fontSize: 13, color: Colors.black26)),
+                            style: TextStyle(fontSize: 13, color: Colors.black),
+                          ),
+                        ),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.search_rounded,
+                              color: Colors.black26,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget navItems({required int items, required Icon icon}) {
+    return IconButton(onPressed: () {}, icon: icon);
   }
 }
