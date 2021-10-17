b26605a90f857a0e14021c09c639af7db08a3727
aurk0 <febtesham162125@bscse.uiu.ac.bd>
2021-10-17 11:56:49 +0600

    categories

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,14 @@ class _StackFoodState extends State<StackFood> {
     'assets/carousel2.JPG',
     'assets/carousel3.JPG'
   ];
+  final List<String> cats = [
+    'assets/cats/cat1.png',
+    'assets/cats/cat2.png',
+    'assets/cats/cat3.png',
+    'assets/cats/cat4.png',
+    'assets/cats/cat5.png',
+    'assets/cats/cat6.png'
+  ];
   int activeindeX = 0;
   @override
   Widget build(BuildContext context) {
@@ -71,7 +79,7 @@ class _StackFoodState extends State<StackFood> {
               child: Container(
                 //color: Colors.white,
                 height: 45,
-                width: 330,
+                width: 350,
                 child: Padding(
                   padding: EdgeInsets.symmetric(horizontal: 15),
                   child: Row(
@@ -135,6 +143,182 @@ class _StackFoodState extends State<StackFood> {
                     activeIndex: activeindeX,
                     count: images.length)
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                    child: Text(
+                      'View All',
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.orange,
+                          decoration: TextDecoration.underline),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Material(
+                          elevation: 10,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            color: Colors.white,
+                            height: 45,
+                            width: 45,
+                            child: Image(
+                              image: AssetImage('assets/cats/cat1.png'),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('All')
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Material(
+                          elevation: 10,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            color: Colors.white,
+                            height: 45,
+                            width: 45,
+                            child: Image(
+                              image: AssetImage('assets/cats/cat2.png'),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Coffee')
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Material(
+                          elevation: 10,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            color: Colors.white,
+                            height: 45,
+                            width: 45,
+                            child: Image(
+                              image: AssetImage('assets/cats/cat3.png'),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Drink')
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Material(
+                          elevation: 10,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            color: Colors.white,
+                            height: 45,
+                            width: 45,
+                            child: Image(
+                              image: AssetImage('assets/cats/cat4.png'),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Fast Food')
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Material(
+                          elevation: 10,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            color: Colors.white,
+                            height: 45,
+                            width: 45,
+                            child: Image(
+                              image: AssetImage('assets/cats/cat5.png'),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Cake')
+                      ],
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Material(
+                          elevation: 10,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            color: Colors.white,
+                            height: 45,
+                            width: 45,
+                            child: Image(
+                              image: AssetImage('assets/cats/cat6.png'),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Sushi')
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             )
           ],
         ),
